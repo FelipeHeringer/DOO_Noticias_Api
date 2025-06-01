886eefe39bf7e26b13c8507ed21863cf152b23f7
Felipe Heringer <heringerfelipe14@gmail.com>
2025-06-01 14:48:54 -0300

    Modificado classe main

--- doo_noticias_api/src/main/java/com/fag/doo_noticias_api/Main.java
+++ doo_noticias_api/src/main/java/com/fag/doo_noticias_api/Main.java
@@ -1,7 +1,19 @@
 package com.fag.doo_noticias_api;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import com.fag.doo_noticias_api.gui.NewsAppGUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            new NewsAppGUI();
+        });
     }
 }
